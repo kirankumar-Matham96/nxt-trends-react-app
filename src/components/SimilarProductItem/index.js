22f025a54aa05fe55db93f7f62fe76a271f33b07
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2022-06-28 20:05:49 +0000

    issue: when clicked similar product item, the component is not updating even though  url is changing

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -1,29 +1,37 @@
+import {Link} from 'react-router-dom'
 import './index.css'
 
 const SimilarProductItem = props => {
   const {productDetails} = props
-  const {title, brand, imageUrl, rating, price} = productDetails
+  const {id, title, brand, imageUrl, rating, price} = productDetails
+
+  //   const onClickSimilarProduct = () => {
+  //     const {history} = props
+  //     history.push(`/products/${id}`)
+  //   }
 
   return (
     <li className="similar-product-item">
-      <img
-        src={imageUrl}
-        className="similar-product-img"
-        alt={`similar product ${title}`}
-      />
-      <p className="similar-product-title">{title}</p>
-      <p className="similar-products-brand">by {brand}</p>
-      <div className="similar-product-price-rating-container">
-        <p className="similar-product-price">Rs {price}/-</p>
-        <div className="similar-product-rating-container">
-          <p className="similar-product-rating">{rating}</p>
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-            alt="star"
-            className="similar-product-star"
-          />
+      <Link to={`/products/${id}`} className="link-item">
+        <img
+          src={imageUrl}
+          className="similar-product-img"
+          alt={`similar product ${title}`}
+        />
+        <p className="similar-product-title">{title}</p>
+        <p className="similar-products-brand">by {brand}</p>
+        <div className="similar-product-price-rating-container">
+          <p className="similar-product-price">Rs {price}/-</p>
+          <div className="similar-product-rating-container">
+            <p className="similar-product-rating">{rating}</p>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+              alt="star"
+              className="similar-product-star"
+            />
+          </div>
         </div>
-      </div>
+      </Link>
     </li>
   )
 }
